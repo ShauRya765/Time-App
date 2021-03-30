f33c4588ce9b427c925603fb6da905b6085db262
ShauRya765 <73055904+ShauRya765@users.noreply.github.com>
2021-03-30 13:22:53 +0530

    second commit

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import React ,{useState} from "react";
+
+
+
+function App() {
+ 
+  setInterval(updateTime,1000);
+  const now = new Date().toLocaleTimeString();
+  const [time ,setTime]=useState(now);
+ 
+  function updateTime(){
+    const newTime = new Date().toLocaleTimeString();
+    setTime(newTime);
+  }
+  
+  return (
+    <div className="container">
+      <h1>{now}</h1>
+
+    </div>
+  );
+}
+
+export default App;
